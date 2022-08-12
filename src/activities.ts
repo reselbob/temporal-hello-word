8dad7e2fe69f2b12362ffc0f0d477cbdf2a06686
Bob Reselman <bob@CogArtTech.com>
2022-08-11 18:02:04 -0700

    added joke feature

--- src/activities.ts
+++ src/activities.ts
@@ -1,5 +1,15 @@
+import axios from 'axios';
+
 // @@@SNIPSTART typescript-hello-activity
 export async function greet(name: string): Promise<string> {
-  return `Greetings, ${name}!`;
+
+  const url = 'https://icanhazdadjoke.com/';
+  console.log(`Getting a joke at URL, ${url} at ${Date.now()}`);
+  const response = await axios.get(url);
+  console.log(`Got a joke at URL, ${response.data} at ${Date.now()}`);
+
+  return `Greetings, ${name}! Here is a joke for you: ${response.data}.`;
 }
 // @@@SNIPEND
+
+
