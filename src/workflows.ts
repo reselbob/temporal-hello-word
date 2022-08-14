6542f03f484641cb70e292daf2b30abc688fe3a0
Bob Reselman <bob@CogArtTech.com>
2022-08-13 17:49:17 -0700

    upgrading package.json

--- src/workflows.ts
+++ src/workflows.ts
@@ -1,20 +1,15 @@
 // @@@SNIPSTART typescript-hello-workflow
-import { proxyActivities } from '@temporalio/workflow';
+//import { proxyActivities } from '@temporalio/workflow';
+import * as wf from '@temporalio/workflow';
 import { sleep } from "@temporalio/workflow";
 // Only import the activity types
 import type * as activities from './activities';
 
-const { greet } = proxyActivities<typeof activities>({
+const { greet } = wf.proxyActivities<typeof activities>({
   startToCloseTimeout: '1 minute',
 });
 
-/** A workflow that calls an activity to times every 2 seconds*/
-export async function example(name: string): Promise<void> {
-  let cnt = 0;
-  while (cnt < 10) {
-    await greet(name);
-    cnt++;
-    await sleep('2s');
-  }
-  return;
+// A workflow function that returns a string
+export async function example(name: string): Promise<string> {
+  return await greet(name);
 }
