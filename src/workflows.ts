86567e1a0117c791b47c29fb401d438623052738
Bob Reselman <bob@CogArtTech.com>
2022-09-05 10:02:04 -0700

    commenting files

--- src/workflows.ts
+++ src/workflows.ts
@@ -3,9 +3,9 @@ import * as wf from '@temporalio/workflow';
 import type * as activities from './activities';
 
 // Get the greetWithJoke activity function in order to make it
-// available to the workflow
+// available to the workflow.
 const { greetWithJoke } = wf.proxyActivities<typeof activities>({
-  startToCloseTimeout: '4 seconds', //the timeout period for task execution
+  startToCloseTimeout: '4 seconds', //the timeout period for task execution. More info here: https://docs.temporal.io/concepts/what-is-a-start-to-close-timeout/
   retry: {
     backoffCoefficient: 1,
     maximumAttempts: 10, //The number of time to retry
